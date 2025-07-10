dc02cf61f7b3435d2742ba851e34ac4c3fa07232
Lukas Ambry <lambry@wedigital.garden>
2025-07-10 15:26:56 +0200

    fix rebase

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ForumCategoryController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ThreadController;
 use Inertia\Inertia;
@@ -28,22 +29,20 @@ Route::prefix('forum')->name('forums.')->group(function(){
 
     Route::post('{category:id}/threads', [ThreadController::class, 'store'])
         ->name('threads.store')->middleware('auth');
+});
+
 Route::get('/game', [GameController::class, 'index'])->name('game.index');
 
 Route::middleware('auth')->group(function(){
     Route::post('/threads',  [ThreadController::class,  'store'])->name('threads.store');
     Route::post('/messages',[MessageController::class,'store'])->name('messages.store');
-});
-    Route::post('/messages',[MessageController::class,'store'])->name('messages.store')->middleware('auth');
 
-Route::middleware(['auth'])->group(function () {
     Route::get('/blog', [BlogPostController::class, 'index'])->name('blog.index');
 //    Route::middleware(['can:create,App\Models\BlogPost'])->group(function () {
-        Route::get('/blog/create', [BlogPostController::class, 'create'])->name('blog.create');
-        Route::post('/blog', [BlogPostController::class, 'store'])->name('blog.store');
+    Route::get('/blog/create', [BlogPostController::class, 'create'])->name('blog.create');
+    Route::post('/blog', [BlogPostController::class, 'store'])->name('blog.store');
 //    });
 });
-
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
