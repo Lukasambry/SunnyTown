0ea62c7e492b5db42a1c86dc2fe1a9c91951a1c0
Lukas Ambry <lambry@wedigital.garden>
2025-07-20 22:31:13 +0200

    feat: rework seeder to add role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,74 +5,50 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Florian de SunnyTown',
+        // Création des rôles
+        Role::firstOrCreate(['name' => 'admin']);
+        Role::firstOrCreate(['name' => 'user']);
+
+        // Création de l'admin
+        $admin = User::create([
+            'name' => 'Admin de SunnyTown',
             'email' => 'admin@sunnytown.com',
             'password' => Hash::make('password'),
             'email_verified_at' => now(),
         ]);
+        $admin->assignRole('admin');
 
-        User::create([
-            'name' => 'Pierre Bûcheron',
-            'email' => 'pierre.bucheron@sunnytown.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ]);
+        // Création des autres utilisateurs
+        $users = [
+            ['name' => 'Pierre Bûcheron', 'email' => 'pierre.bucheron@sunnytown.com'],
+            ['name' => 'Marie Fermière', 'email' => 'marie.fermiere@sunnytown.com'],
+            ['name' => 'Lucas Mineur', 'email' => 'lucas.mineur@sunnytown.com'],
+            ['name' => 'Sophie Architecte', 'email' => 'sophie.architecte@sunnytown.com'],
+            ['name' => 'Thomas Marchand', 'email' => 'thomas.marchand@sunnytown.com'],
+            ['name' => 'Emma Écologiste', 'email' => 'emma.ecologiste@sunnytown.com'],
+            ['name' => 'Max Industriel', 'email' => 'max.industriel@sunnytown.com'],
+            ['name' => 'Clara Décoratrice', 'email' => 'clara.decoratrice@sunnytown.com'],
+        ];
 
-        User::create([
-            'name' => 'Marie Fermière',
-            'email' => 'marie.fermiere@sunnytown.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ]);
+        foreach ($users as $userData) {
+            $user = User::create([
+                'name' => $userData['name'],
+                'email' => $userData['email'],
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ]);
+            $user->assignRole('user');
+        }
 
-        User::create([
-            'name' => 'Lucas Mineur',
-            'email' => 'lucas.mineur@sunnytown.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ]);
-
-        User::create([
-            'name' => 'Sophie Architecte',
-            'email' => 'sophie.architecte@sunnytown.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ]);
-
-        User::create([
-            'name' => 'Thomas Marchand',
-            'email' => 'thomas.marchand@sunnytown.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ]);
-
-        User::create([
-            'name' => 'Emma Écologiste',
-            'email' => 'emma.ecologiste@sunnytown.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ]);
-
-        User::create([
-            'name' => 'Max Industriel',
-            'email' => 'max.industriel@sunnytown.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ]);
-
-        User::create([
-            'name' => 'Clara Décoratrice',
-            'email' => 'clara.decoratrice@sunnytown.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ]);
-
-        User::factory(15)->create();
+        // Utilisateurs générés par la factory
+        User::factory(15)->create()->each(function ($user) {
+            $user->assignRole('user');
+        });
     }
 }
